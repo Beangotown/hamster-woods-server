7c73f408fd90177155ca9ddfae61920ec5d71c1f
felix03zhao <cici0306@beangotown.com>
2024-07-26 20:00:58 +0800

    modify: sync record period.

--- src/HamsterWoods.Application/Options/BackgroundJobOptions.cs
+++ src/HamsterWoods.Application/Options/BackgroundJobOptions.cs
@@ -2,5 +2,5 @@ namespace HamsterWoods.Options;
 
 public class BackgroundJobOptions
 {
-    public string SyncRecordCorn { get; set; } = "0 0 0/1 * * ?";
+    public string SyncRecordCorn { get; set; } = "0 0 0 * * ?";
 }
