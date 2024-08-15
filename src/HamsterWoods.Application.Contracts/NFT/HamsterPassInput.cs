165eee578eaa7db44c40fe69e255135b27ef908a
felix03zhao <cici0306@beangotown.com>
2024-08-15 10:58:32 +0800

    modify: join points.

--- src/HamsterWoods.Application.Contracts/NFT/HamsterPassInput.cs
+++ src/HamsterWoods.Application.Contracts/NFT/HamsterPassInput.cs
@@ -4,6 +4,7 @@ public class HamsterPassInput
 {
     public string CaAddress { get; set; }
     public int WeekNum { get; set; }
+    public string Domain { get; set; }
 }
 
 public class GetHamsterPassInput : HamsterPassInput
