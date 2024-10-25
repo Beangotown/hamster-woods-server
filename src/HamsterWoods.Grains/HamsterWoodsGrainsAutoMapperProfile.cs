d1deba9d5befc9ca460389d57d4f83c586f6be3b
Edward-BXS <Magic.Wizard@portkey.finance>
2024-10-25 18:35:29 +0800

    add HealthCheckState mapping config

--- src/HamsterWoods.Grains/HamsterWoodsGrainsAutoMapperProfile.cs
+++ src/HamsterWoods.Grains/HamsterWoodsGrainsAutoMapperProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
+using HamsterWoods.Grains.Grain.HealthCheck;
 using HamsterWoods.Grains.Grain.Points;
 using HamsterWoods.Grains.Grain.Unlock;
+using HamsterWoods.Grains.State.HealthCheck;
 using HamsterWoods.Grains.State.Points;
 using HamsterWoods.Grains.State.Unlock;
 
@@ -13,5 +15,6 @@ public class HamsterWoodsGrainsAutoMapperProfile : Profile
         CreateMap<ContractInvokeState, ContractInvokeGrainDto>().ReverseMap();
         CreateMap<PointsInfoGrainDto, PointsInfoState>().ReverseMap();
         CreateMap<UnlockInfoState, UnlockInfoGrainDto>().ReverseMap();
+        CreateMap<HealthCheckGrainDto, HealthCheckState>().ReverseMap();
     }
 }
